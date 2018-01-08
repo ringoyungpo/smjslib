90d2972f66d0afd97d79aac005f568d385b19bba
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-01-08 21:29:10 +0800

    add pages and lenth ,change associations to association

--- smjslib/spiders/books.py
+++ smjslib/spiders/books.py
@@ -2,6 +2,7 @@
 import scrapy
 import re
 
+
 def parse_books_imformation(response):
     book_info = response.css('#ctl00_ContentPlaceHolder1_bookcardinfolbl').xpath('string(.)').extract()[0].split(
         '\u3000')
@@ -12,14 +13,19 @@ def parse_books_imformation(response):
     author = title_author_publishment[1]
     publishment = title_author_publishment[-1]
     publisher_city, publisher, publish_year = re.split('：|，', publishment)
-    publish_year = re.search(r'\d{4}',publish_year).group(0)
+    publish_year = re.search(r'\d{4}', publish_year).group()
+
+    pages_length = re.split('；', book_info[1])
+    pages = re.findall('\d\d+', pages_length[0])
+    pages = max(pages)
+    length = re.search('\d\d+', pages_length[1]).group()
 
     isbnandprice = str(book_info[-5])
     isbn = isbnandprice.split('：')[0]
-    isbn = re.search(r'ISBN[\w|-]+', isbn).group(0)
+    isbn = re.search(r'ISBN[\w|-]+', isbn).group()
 
     price = isbnandprice.split('：')[1]
-    price = re.search(r'\d+.?\d+', price).group(0)
+    price = re.search(r'\d+.?\d+', price).group()
 
     titles = str(book_info[-4]).split('．')[-1]
     titles = re.split(r'[①-⑳]', titles)
@@ -37,20 +43,18 @@ def parse_books_imformation(response):
     tags = sum(tags, [])
     tags = list(set(tags))
 
-    associations = str(book_info[-1]).split('．')[-1][1:]
-    associations = re.split(r'[①-⑳]', associations)
-    associations = list(map(lambda association: re.search(r'\w+[.\w+]+', association).group(0), associations))
-    associations = list(filter(lambda association: association, associations))
+    association = response.xpath('//*[@id="bardiv"]/div/table/tbody/tr[1]/td[2]/text()').extract()[0].split('/')[0]
+    association = re.search(r'\w+[.\w+]+[-\w+]+', association).group()
 
     total = len(response.xpath('//*[@id="bardiv"]/div/table/tbody/tr[*]/td[1]/a'))
     loan = len(response.xpath('//*[@id="bardiv"]/div/table/tbody/tr[*]/td[6]/a'))
     frequence = response.xpath('//*[@id="ctl00_ContentPlaceHolder1_blclbl"]/text()').extract()[0]
 
-    if isbn and price:
-        yield {'title': title, 'author': author, 'publisher_city': publisher_city, 'publisher': publisher,
-               'publish_year': publish_year, 'isbn': isbn, 'price': price, 'titles': titles, 'authors': authors,
-               'tags': tags, 'associations': associations, 'total': total, 'available': total - loan, 'loan': loan,
-               'frequence': frequence}
+    if isbn and price and association:
+        yield dict(title=title, author=author, publisher_city=publisher_city, publisher=publisher,
+                   publish_year=publish_year, pages=pages, length=length, isbn=isbn, price=price, titles=titles,
+                   authors=authors, tags=tags,
+                   association=association, total=total, available=total - loan, loan=loan, frequence=frequence)
 
 
 def parse_books_url(response):
