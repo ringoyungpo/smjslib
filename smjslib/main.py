aec4225e955523dd7586080d7dd92d8a1631c3a7
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-01-08 14:11:10 +0800

    chage the format of output name to seconds ending

--- smjslib/main.py
+++ smjslib/main.py
@@ -1,6 +1,6 @@
 from scrapy import cmdline
 import time
 
-localtime = time.strftime('%Y-%m-%d_%H:%M',time.localtime(time.time()))
+localtime = time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time()))
 
 cmdline.execute('scrapy crawl books -o books-{}.csv'.format(localtime).split())
