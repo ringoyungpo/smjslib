4add6aa84f688b07d744c563f3a9478b2df1823c
Ringo Yungpo Kao <ringoyungpo@qq.com>
2018-01-09 13:49:19 +0800

    port data into mysql smjslib book table

--- smjslib/main.py
+++ smjslib/main.py
@@ -1,6 +1,7 @@
 from scrapy import cmdline
 import time
 
-localtime = time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time()))
+# localtime = time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(time.time()))
 
-cmdline.execute('scrapy crawl books -o books-{}.csv'.format(localtime).split())
+# cmdline.execute('scrapy crawl books -o books-{}.csv'.format(localtime).split())
+cmdline.execute('scrapy crawl books'.split())
--- smjslib/spiders/books.py
+++ smjslib/spiders/books.py
@@ -2,6 +2,7 @@
 import scrapy
 import re
 from urllib.parse import urlencode
+from smjslib.items import SmjslibItem
 
 
 def parse_books_imformation(response):
@@ -50,12 +51,26 @@ def parse_books_imformation(response):
     total = len(response.xpath('//*[@id="bardiv"]/div/table/tbody/tr[*]/td[1]/a'))
     loan = len(response.xpath('//*[@id="bardiv"]/div/table/tbody/tr[*]/td[6]/a'))
     frequence = response.xpath('//*[@id="ctl00_ContentPlaceHolder1_blclbl"]/text()').extract()[0]
-
+    item = SmjslibItem()
     if isbn and price and association:
-        yield dict(title=title, author=author, publisher_city=publisher_city, publisher=publisher,
-                   publish_year=publish_year, pages=pages, length=length, isbn=isbn, price=price, titles=titles,
-                   authors=authors, tags=tags,
-                   association=association, total=total, available=total - loan, loan=loan, frequence=frequence)
+        item['title'] = title
+        item['author'] = author
+        item['publisher_city'] = publisher_city
+        item['publisher'] = publisher
+        item['publish_year'] = publish_year
+        item['pages'] = pages
+        item['length'] = length
+        item['isbn'] = isbn
+        item['price'] = price
+        item['titles'] = titles
+        item['authors'] = authors
+        item['tags'] = tags
+        item['association'] = association
+        item['total'] = total
+        item['available'] = total - loan
+        item['loan'] = loan
+        item['frequence'] = frequence
+        yield item
 
 
 def parse_books_url(response):
